1acee6e6cf7a56185a25c98dfc90bbcd15b6ca3c
RimaAz <90001216+RimaAz@users.noreply.github.com>
2021-10-11 22:05:24 -0400

    complet

--- exercice.py
+++ exercice.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 def majuscule(mot):
-    # TODO completer la fonction ici
-    return mot
+    mot_maj= ""
+    for lettre in mot:
+        mot_maj += chr(ord(lettre)-32)
+
+    return mot_maj
+
 
 
 if __name__ == '__main__':
@@ -13,7 +17,8 @@ if __name__ == '__main__':
         'oiseau',
         'bonjour',
         'églantier',
-        'arbre'
+        'arbre',
+        'yolo'
     ]
     for i in range(len(mots)):
         mots[i] = majuscule(mots[i])
